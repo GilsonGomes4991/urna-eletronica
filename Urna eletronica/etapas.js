add99848cb45d453e6ef32fd7fa297f19664a235
Gilson Gomes <94126776+GilsonGomes4991@users.noreply.github.com>
2022-04-18 23:17:09 -0300

    Add files via upload

---
+++ Urna eletronica/etapas.js
@@ -0,0 +1,56 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                name: 'Gilson',
+                partido: 'PR',
+                fotos: [
+                    {url:'Gil.jfif.fif', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: 72222,
+                name: 'Jadh',
+                partido: 'DT',
+                fotos: [
+                {url:'jadh.jpg', legenda: 'Vereadora'}
+                ]
+
+
+
+            },
+            {
+                titulo:'PREFEITO',
+                numero: 2,
+                candidatos: [
+                    {
+                        numero: 71,
+                        name: 'Junior',
+                        partido: 'PRG',
+                        vice: 'Rodrigues',
+                        fotos: [
+                            {url:'Gil.jfif', legenda: 'Prefeito'},
+                            {url:'jadh.jpg', legenda: 'Vice-Prefeita', small: true},
+                        ]
+                    },
+                    {
+                        numero: 33,
+                        name: 'Rodrigues',
+                        partido: 'DTS',
+                        vice: 'Gil',
+                        fotos: [
+                            {url:'jadh.jpg', legenda: 'Prefeita'},
+                            {url:'Gil.jfif', legenda: 'Vice-Prefeito',  small: true}
+                        ]
+                    },
+                ]
+            }
+        ]
+    }
+];
+        
+           
+
